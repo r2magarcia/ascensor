954233aedf4331c59aaacdf79caccbc1bcb386a1
r2magarcia <ma.garcia2r@gmail.com>
2021-11-03 11:26:07 -0500

    ya casi

--- prueba9.js
+++ prueba9.js
@@ -3,8 +3,12 @@ let pisoIni=5;
 let mapaPisos={};
 let sentido="";
 let pisoActual=0;
+let recorrido=[];
 
-
+/**
+ * @param {*} llave piso en el que se ingresa
+ * @param {*} valor nuevo piso ingresado
+ */
 function añadirPiso(llave, valor) {
     arrayPisos.push(llave);
     mapaPisos[llave]=valor;
@@ -12,15 +16,66 @@ function añadirPiso(llave, valor) {
 
 function definirSentido() {
     sentido = pisoIni<arrayPisos[0]? "subiendo" : "bajando";
-    console.log(sentido);
-    
+}
+
+function ordenInicial() {
+    pisoActual=pisoIni;
+    recorrido=[...arrayPisos];
+    recorrido.sort();
+}
+
+function detenerElevador() {
+   console.log(`Elevador en piso ${pisoActual}`);
+   console.log(`Elevador se detiene`);
+   recorrido = recorrido.filter(function(e) { return e !== pisoActual })
+   if(mapaPisos[pisoActual]){
+        console.log(`Piso ingresado ${mapaPisos[pisoActual]}`);   
+        recorrido.push(mapaPisos[pisoActual]);
+        recorrido.sort();
+   }
+}
+
+function moverse(final) {
+    if(sentido=="subiendo"){
+        for (index=pisoActual; index < final; ) {
+            console.log(`Elevador en piso ${pisoActual}`);
+            console.log(`Elevador ${sentido}`);
+            pisoActual++;
+        } 
+    }else{
+        for (index=pisoActual; pisoActual > final; ) {
+            console.log(`Elevador en piso ${pisoActual}`);
+            console.log(`Elevador ${sentido}`);
+            pisoActual--;  
+        }
+    }
+    detenerElevador();
+}
+
+function elevador() {
+    ordenInicial();
+    definirSentido();
+    // console.log(arrayPisos[0]);
+    console.log(recorrido.findIndex(e=>e==arrayPisos[0]));
+    console.log(recorrido);
+    if(sentido=="subiendo"){
+        console.log("subir");
+        for (let index = recorrido.findIndex(e=>e==arrayPisos[0]); index <= recorrido.length-1; index++) {
+            console.log(recorrido[index]);
+            moverse(recorrido[index]); 
+        }
+    }else{
+        console.log("bajar");
+        for (let index = recorrido.findIndex(e=>e==arrayPisos[0]); index >= 0; index--) {
+            moverse(recorrido[index]); 
+        }
+    }      
 }
 
 añadirPiso(2,5);
+añadirPiso(1,20);
 añadirPiso(3,1);
 añadirPiso(7,9);
 console.log(arrayPisos);
-console.log(mapaPisos);
-console.log(definirSentido());
-console.log(Reflect.ownKeys(mapaPisos));
+elevador();
 
